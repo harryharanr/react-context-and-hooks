5ad844db3f4b995ab45bd2665c09d29a410ec60a
Hariharan Ramanan <harryharanr@gmail.com>
2020-05-19 12:36:04 +0530

    Working with multiple contexts

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -5,7 +5,7 @@ class ThemeToggle extends Component {
   static contextType = ThemeContext;
   render() {
     const { toggleTheme } = this.context;
-    return <button onClick={toggleTheme}> Toggle Theme</button>;
+    return <button onClick={toggleTheme}>Toggle Theme</button>;
   }
 }
 
